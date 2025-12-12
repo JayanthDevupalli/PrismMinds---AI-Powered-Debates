ddd0386c6a222e14953207540920c66de09437ca
Jayanth <devupallijayanth@gmail.com>
2025-12-12 23:55:30 +0530

    error check

--- prismminds-server/server.js
+++ prismminds-server/server.js
@@ -19,12 +19,12 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options("*", cors({
-  origin: "https://prismminds.vercel.app",
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// app.options("*", cors({
+//   origin: "https://prismminds.vercel.app",
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 
 
 app.use(express.json());
