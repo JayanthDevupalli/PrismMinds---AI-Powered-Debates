6f652c65ca9c1474af342b6e8df2f5e86dfa8cf2
Jayanth <devupallijayanth@gmail.com>
2025-12-12 22:20:17 +0530

    productionupdate - version 1.243

--- prismminds-server/config/firebaseAdmin.js
+++ prismminds-server/config/firebaseAdmin.js
@@ -1,17 +1,42 @@
+// // config/firebaseAdmin.js
+// import admin from "firebase-admin";
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+
+// const serviceAccount = require("./serviceAccountKey.json");
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
+
+// const auth = admin.auth(); // ✅ create the auth instance
+// const db = admin.firestore(); // (optional for later use)
+
+// export { admin, auth, db }; // ✅ export all
+
+
+
+
+
 // config/firebaseAdmin.js
 import admin from "firebase-admin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
-const serviceAccount = require("./serviceAccountKey.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key: process
+        .env
+        .FIREBASE_PRIVATE_KEY
+        ?.replace(/\\n/g, "\n"),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
 }
 
-const auth = admin.auth(); // ✅ create the auth instance
-const db = admin.firestore(); // (optional for later use)
+const auth = admin.auth();
+const db = admin.firestore();
 
-export { admin, auth, db }; // ✅ export all
+export { admin, auth, db };
