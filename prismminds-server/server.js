bd6f7dc3a68d608519d6f181a8e3854ba7f5ce14
Jayanth <devupallijayanth@gmail.com>
2025-12-12 21:19:35 +0530

    productionupdate - version 1.24

--- prismminds-server/server.js
+++ prismminds-server/server.js
@@ -10,7 +10,8 @@ const app = express();
 
 // Enhanced CORS configuration
 app.use(cors({ 
-  origin: "http://localhost:3000",
+  // origin: "http://localhost:3000",
+  origin: "https://prismminds.vercel.app",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
