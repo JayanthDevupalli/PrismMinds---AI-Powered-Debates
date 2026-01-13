e9f26e533bce1ef20211b31d83a853bf3797b200
Jayanth <devupallijayanth@gmail.com>
2026-01-13 19:02:27 +0530

    Integrated Support Chatbot - Version 2.22

--- prismmindsclient/components/ChatBot.tsx
+++ prismmindsclient/components/ChatBot.tsx
@@ -96,7 +96,7 @@ export default function ChatBot() {
             }
 
             // Call Backend
-            const response = await fetch("http://prismmindsdb.onrender.com/api/chat", {
+            const response = await fetch("https://prismmindsdb.onrender.com/api/chat", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
